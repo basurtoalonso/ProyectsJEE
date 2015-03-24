d5b6bf0bd8b22bdfd1126cacdc73a27fab1885a4
basurtoalonso <victor.basurto@hotmail.com>
2015-03-24 13:17:15 -0600

    Create Constantes.java

---
+++ Constantes.java
@@ -0,0 +1,14 @@
+package com.bancoazteca.ebanking.web.bean.ws;
+
+public class Constantes {
+	//CONSTANTES PARA RESPUESTA DEL SERVICIO
+	public static final String MENSAJE_FAILED_INVOCA_CASO_DAO = "NO SE PUDO REGISTRAR EL CASO";
+	public static final String MENSAJE_OK_INVOCA_CASO_DAO = "SE REGISTRO EL CASO CORRECTAMENTE: IDCASO = ";
+	public static final String MENSAJE_FAILED_CREATE_CASO = "OCURRIO UNA EXCEPCION CUANDO SE INTENTABA ARMAR EL OBJETO CASO";
+	public static final String CODIGO_FAILED_DAO = "070600";
+	public static final String CODIGO_OK_DAO = "070601";
+	public static final String CODIGO_FAILED_CREATE = "070602";
+	public static final String ESTATUS_OK = "OK";
+	public static final String ESTATUS_FAILED = "FAILED";
+	public static final String CODIGO_FAILED_CONSULTA = "070603";
+}
